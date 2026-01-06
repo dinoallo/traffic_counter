c929bb5b92dd526fc89f5a24f8ce67a28d0607d4
Yun Pan <dinoallo@netc.it>
2026-01-06 12:12:42 +0800

    fix: attempt to fix broken build on wasm32 targets
    Signed-off-by: Yun Pan <dinoallo@netc.it>

--- api/build.rs
+++ api/build.rs
@@ -1,8 +1,15 @@
-
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    tonic_build::configure()
-        .build_server(true)
-        .build_client(true)
-        .compile(&["proto/traffic.proto"], &["proto"])?;
+    let target_arch = std::env::var("CARGO_CFG_TARGET_ARCH").unwrap_or_default();
+    if target_arch == "wasm32" {
+        tonic_build::configure()
+            .build_server(false)
+            .build_client(false)
+            .compile(&["proto/traffic.proto"], &["proto"])?;
+    } else {
+        tonic_build::configure()
+            .build_server(true)
+            .build_client(true)
+            .compile(&["proto/traffic.proto"], &["proto"])?;
+    }
     Ok(())
 }
